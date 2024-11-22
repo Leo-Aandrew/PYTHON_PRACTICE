b808c5ac798b2f9ba6910c8579ce69c2f4cad53b
Leo-Aandrew <153619876+Leo-Aandrew@users.noreply.github.com>
2024-11-22 22:18:33 +0530

    Update class_object.py

--- PYTHON PRACTICE/class_object.py
+++ PYTHON PRACTICE/class_object.py
@@ -1,4 +1,5 @@
 ### SIMPLE USAGE OF CLASS AND OBJECT ###
+### output: 11 ###
 class A():
     def sum(self,a,b):
         c=a+b
@@ -6,3 +7,25 @@ class A():
 
 obj=A()
 obj.sum(2,9)
+
+
+### linkedlist ###
+### output: 1-->2-->3 ###
+class linkedlist:
+    def __init__(self,num):
+        self.node=num
+        self.next=None
+
+ob1=linkedlist(1)
+ob2=linkedlist(2)
+ob3=linkedlist(3)
+ob1.next=ob2
+ob2.next=ob3
+
+def printlist(head):
+    current=head
+    while current is not None:
+        print(current.node,end="-->" if current.next else "\n")
+        current=current.next
+
+printlist(ob1)
